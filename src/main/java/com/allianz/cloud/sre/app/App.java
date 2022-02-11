6f69591a4f589ccf0ad0c357db95764b40618bb6
Ualter Otoni <ualter.otoni@allianz.es>
2022-02-11 10:04:53 +0100

    CLOUDOPS-782

--- src/main/java/com/allianz/cloud/sre/app/App.java
+++ src/main/java/com/allianz/cloud/sre/app/App.java
@@ -10,15 +10,4 @@ public class App {
 		SpringApplication.run(App.class, args);
 	}
 
-	// @Bean
-	// public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-	// 	return args -> {
-	// 		String[] beanNames = ctx.getBeanDefinitionNames();
-	// 		Arrays.sort(beanNames);
-	// 		for (String beanName : beanNames) {
-	// 			System.out.println(beanName);
-	// 		}
-	// 	};
-	// }
-
 }
